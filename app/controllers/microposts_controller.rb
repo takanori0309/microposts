6e00692109f6326a4cb471f56186ae3f7d4c234a
takanoritomoe <takanori0309_@i.softbank.jp>
2017-04-08 17:25:27 +0000

    add home view's timelone

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,11 +7,18 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
       render 'static_pages/home'
     end
   end
   
-  
+  def destroy
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    return redirect_to root_url if @micropost.nil?
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || root_url
+  end
   
   private
   def micropost_params
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   resources :microposts
+  resources :relationships, only: [:create, :destroy]
 end
